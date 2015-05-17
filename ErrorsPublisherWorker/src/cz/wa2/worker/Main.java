f56d8d434e10bc670ec841512973d9d8e43a4568
Bc. Tomáš Tisančín <tisantom@fel.cvut.cz>
2015-05-17 12:35:12 +0200

    Poison Ivy

--- ErrorsPublisherWorker/src/cz/wa2/worker/Main.java
+++ ErrorsPublisherWorker/src/cz/wa2/worker/Main.java
@@ -1,13 +1,13 @@
 package cz.wa2.worker;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -15,11 +15,13 @@ import org.json.JSONObject;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 import com.rabbitmq.client.QueueingConsumer;
 
 public class Main {
 
 	private static final String PUBLISH_ERRORS_TASK_QUEUE = "publish_error";
+	private static final String POISONED_TASK_QUEUE = "poison_queue";
 
 	public static void main(String[] argv)
 			throws java.io.IOException,
@@ -43,14 +45,16 @@ public class Main {
 			String message = new String(delivery.getBody());
 
 			System.out.println(" [x] Received '" + message + "'");
-			doWork(message);
+			try {
+				doWork(message);
+			} catch (WorkerException e) {
+				arrrrItsPoisoned(message);
+			}
 			System.out.println(" [x] Done");
-
-			//channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
 		}
 	}
 
-	private static void doWork(String message) {
+	private static void doWork(String message) throws WorkerException {
 		JSONObject msg = new JSONObject(message);
 		String uri = msg.getString("uri");
 
@@ -83,6 +87,39 @@ public class Main {
 				writer.close();
 			} catch (Exception e) {
 				e.printStackTrace();
+			}
+		}
+	}
+
+	private static void arrrrItsPoisoned(String message) {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = null;
+		Channel channel = null;
+		try {
+			connection = factory.newConnection();
+			channel = connection.createChannel();
+			channel.queueDeclare(POISONED_TASK_QUEUE, true, false, false, null);
+			channel.basicPublish("", POISONED_TASK_QUEUE,
+					MessageProperties.PERSISTENT_TEXT_PLAIN,
+					message.getBytes());
+			System.out.println("POISONED task queued: " + message + "");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (channel != null) {
+				try {
+					channel.close();
+				} catch (IOException | TimeoutException e) {
+					e.printStackTrace();
+				}
+			}
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
--- ImagePublisherWorker/src/cz/wa2/worker/Main.java
+++ ImagePublisherWorker/src/cz/wa2/worker/Main.java
@@ -3,6 +3,7 @@ package cz.wa2.worker;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.concurrent.TimeoutException;
 
 import org.apache.commons.codec.binary.Base64;
 import org.json.JSONObject;
@@ -10,11 +11,13 @@ import org.json.JSONObject;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 import com.rabbitmq.client.QueueingConsumer;
 
 public class Main {
 
 	private static final String PUBLISH_IMAGE_TASK_QUEUE = "publish_image";
+	private static final String POISONED_TASK_QUEUE = "poison_queue";
 
 	public static void main(String[] argv)
 			throws java.io.IOException,
@@ -36,16 +39,17 @@ public class Main {
 		while (true) {
 			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
 			String message = new String(delivery.getBody());
-
 			System.out.println(" [x] Received '" + message + "'");
-			doWork(message);
+			try {
+				doWork(message);
+			} catch (WorkerException e) {
+				arrrrItsPoisoned(message);
+			}
 			System.out.println(" [x] Done");
-
-			//channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
 		}
 	}
 
-	private static void doWork(String message) {
+	private static void doWork(String message) throws WorkerException {
 		JSONObject msg = new JSONObject(message);
 		Long id = msg.getLong("id");
 		String uri = msg.getString("uri");
@@ -56,6 +60,39 @@ public class Main {
 			stream.write(data);
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+
+	private static void arrrrItsPoisoned(String message) {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = null;
+		Channel channel = null;
+		try {
+			connection = factory.newConnection();
+			channel = connection.createChannel();
+			channel.queueDeclare(POISONED_TASK_QUEUE, true, false, false, null);
+			channel.basicPublish("", POISONED_TASK_QUEUE,
+					MessageProperties.PERSISTENT_TEXT_PLAIN,
+					message.getBytes());
+			System.out.println("POISONED task queued: " + message + "");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (channel != null) {
+				try {
+					channel.close();
+				} catch (IOException | TimeoutException e) {
+					e.printStackTrace();
+				}
+			}
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
