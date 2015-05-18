af20863e1c5bc76f98c4fd102a01e95dd7e12aa0
Pavel Lieberzeit <humungusus@seznam.cz>
2015-05-18 13:34:08 +0200

    zajebany git

--- ErrorsPublisherWorker/src/cz/wa2/worker/Main.java
+++ ErrorsPublisherWorker/src/cz/wa2/worker/Main.java
@@ -7,6 +7,7 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -20,23 +21,26 @@ import org.json.JSONObject;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 import com.rabbitmq.client.QueueingConsumer;
 
 public class Main {
 
 	private static final String PUBLISH_ERRORS_TASK_QUEUE = "publish_error";
-	
-	private static StandardServiceRegistry serviceRegistry;
-    private static SessionFactory sessionFactory;
-    
-    static {
-    	Configuration configuration = new Configuration().configure();
-        serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-        sessionFactory = configuration.configure().buildSessionFactory(serviceRegistry);
-    }
+	private static final String POISONED_TASK_QUEUE = "poison_queue";
 
-	public static void main(String[] argv)
-			throws java.io.IOException,
+	private static StandardServiceRegistry serviceRegistry;
+	private static SessionFactory sessionFactory;
+
+	static {
+		Configuration configuration = new Configuration().configure();
+		serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+				configuration.getProperties()).build();
+		sessionFactory = configuration.configure().buildSessionFactory(
+				serviceRegistry);
+	}
+
+	public static void main(String[] argv) throws java.io.IOException,
 			java.lang.InterruptedException {
 
 		ConnectionFactory factory = new ConnectionFactory();
@@ -44,7 +48,8 @@ public class Main {
 		Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
 
-		channel.queueDeclare(PUBLISH_ERRORS_TASK_QUEUE, true, false, false, null);
+		channel.queueDeclare(PUBLISH_ERRORS_TASK_QUEUE, true, false, false,
+				null);
 		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
 		channel.basicQos(1);
@@ -57,27 +62,31 @@ public class Main {
 			String message = new String(delivery.getBody());
 
 			System.out.println(" [x] Received '" + message + "'");
-			doWork(message);
+			try {
+				doWork(message);
+			} catch (WorkerException e) {
+				arrrrItsPoisoned(message);
+			}
 			System.out.println(" [x] Done");
 
 			channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
 		}
 	}
 
-	private static void doWork(String message) {
+	private static void doWork(String message) throws WorkerException {
 		JSONObject msg = new JSONObject(message);
 		String uri = msg.getString("uri");
 
 		Session session = sessionFactory.openSession();
-        
-        Query query = session.createQuery("FROM Error");
-        
-        List<cz.wa2.entity.Error> errors = query.list();
-        
-        session.close();
-		
+
+		Query query = session.createQuery("FROM Error");
+
+		List<cz.wa2.entity.Error> errors = query.list();
+
+		session.close();
+
 		JSONArray data = new JSONArray();
-		
+
 		for (cz.wa2.entity.Error e : errors) {
 			JSONArray row = new JSONArray();
 			row.put(e.getId());
@@ -86,16 +95,25 @@ public class Main {
 			row.put(e.getPage().getApplication().toString());
 			row.put(e.getUser().toString());
 			row.put(e.getComment());
-			// Tohle by se melo generovat na klientovi, ale takhle je to jednodussi :-)
-			row.put("<button type='button' id='" + e.getId() + "'>SHOW SCREENSHOT</button>");
-			row.put("<button type='button' id='" + e.getId() + "'>CANCEL</button>");
-			row.put("<button type='button' id='" + e.getId() + "'>RESOLVE</button>");
+			// Tohle by se melo generovat na klientovi, ale takhle je to
+			// jednodussi :-)
+
+			row.put("<button type='button' class='screenshot' id='" + e.getId()
+					+ "'>SHOW SCREENSHOT</button>");
+			row.put("<button type='button'"
+					+ (e.isCanceled() ? "disabled='disabled'" : "")
+					+ " class='cancel' id='" + e.getId() + "'>CANCEL</button>");
+			row.put("<button type='button'"
+					+ (e.isResolved() ? "disabled='disabled'" : "")
+					+ " class='resolve' id='" + e.getId()
+					+ "'>RESOLVE</button>");
 			data.put(row);
 		}
 		String publishableData = data.toString();
 		Writer writer = null;
 		try {
-			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(uri), "utf-8"));
+			writer = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream("../publish" + uri), "utf-8"));
 			writer.write(publishableData);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -104,6 +122,38 @@ public class Main {
 				writer.close();
 			} catch (Exception e) {
 				e.printStackTrace();
+			}
+		}
+	}
+
+	private static void arrrrItsPoisoned(String message) {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = null;
+		Channel channel = null;
+		try {
+			connection = factory.newConnection();
+			channel = connection.createChannel();
+			channel.queueDeclare(POISONED_TASK_QUEUE, true, false, false, null);
+			channel.basicPublish("", POISONED_TASK_QUEUE,
+					MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
+			System.out.println("POISONED task queued: " + message + "");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (channel != null) {
+				try {
+					channel.close();
+				} catch (IOException | TimeoutException e) {
+					e.printStackTrace();
+				}
+			}
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
